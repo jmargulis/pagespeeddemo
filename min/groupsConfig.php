2f2334bd7223a367b6aa010d4655a4464da0c09c
Josh Margulis <josh@honeyfund.com>
2014-08-16 01:04:14 -0700

    Minify JS and CSS
    Had to adjust some of the <script> and <style> tags to work in both
    scenarios. Some @imports in CSS files had to be explicitly added to the
    min/groupsConfig.php file Also added a minify-cache directory at the
    root; and set permissions to let Apache write to it.

---
+++ min/groupsConfig.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Groups configuration for default Minify implementation
+ * @package Minify
+ */
+
+/** 
+ * You may wish to use the Minify URI Builder app to suggest
+ * changes. http://yourdomain/min/builder/
+ *
+ * See http://code.google.com/p/minify/wiki/CustomSource for other ideas
+ **/
+
+return array(
+	'js' => array(
+		"//js/jquery-1.7.2.min.js"
+		,"//js/swfobject.js"
+		,"//js/modernizr.js"
+		,"//js/jquery.elastislide.js"
+		,"//js/jflickrfeed.js"
+		,"//js/custom.js"
+		,"//js/bootstrap.min.js"
+		,"//js/jquery.mobile.customized.min.js"
+		,"//js/jquery.form.min.js"
+		,"//js/scripts.js"
+		,"//js/superfish.js"
+		,"//js/jquery.mobilemenu.js"
+		,"//js/jquery.easing.1.3.js"
+		,"//js/jquery.magnific-popup.min.js"
+		,"//js/jquery.flexslider.js"
+		,"//js/jplayer.playlist.min.js"
+		,"//js/jquery.jplayer.min.js"
+		,"//js/camera.min.js"
+		,"//js/add-to-cart.min.js"
+		,"//js/jquery.blockUI.min.js"
+		,"//js/woocommerce.min.js"
+		,"//js/jquery.cookie.min.js"
+		,"//js/cart-fragments.min.js"
+		,"//js/jquery.placeholder.min.js"
+		,"//js/custom-script.js"
+		,"//js/my-custom-script.js"
+	),
+	'css' => array(
+		"//css/bootstrap.css"
+		,"//css/responsive.css"
+		,"//css/camera.css"
+		,"//CherryFramework/style.css"
+		,"//css/main-style.css"
+		,"//css/styles.css"
+		,"//css/font-awesome.css"
+		,"//css/magnific-popup.css"
+		,"//css/woocommerce.css"
+	)
+    // 'js' => array('//js/file1.js', '//js/file2.js'),
+    // 'css' => array('//css/file1.css', '//css/file2.css'),
+);
